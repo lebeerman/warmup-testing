d4987754846cf88a2659a5c55e9fa16bafba3034
dan <omnidbeerman@gmail.com>
2017-12-04 09:52:19 -0700

    there you go

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,8 @@ const app = require('../app');
 
 describe('Welcome to the TestingDome', ()=>{
   describe('#First Test: There should be a function called isFun()',()=>{
-
+    it('should return true!', ()=>{
+      assert.equal(app.isFun(), true);
+    })
   });
 });
