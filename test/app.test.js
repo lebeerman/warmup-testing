b05308a06f413876f144814ae14242b7d104de2a
dan <omnidbeerman@gmail.com>
2017-12-04 09:46:20 -0700

    1 simple test added

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const app = require('../app');
+
+
+describe('Welcome to the TestingDome', ()=>{
+  describe('#First Test: There should be a function called isFun()',()=>{
+
+  });
+});
